d823dd348097b719df5efd9a3e708920b08d51a7
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-15 17:50:11 +0200

    add file

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EnterCorrectPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();                                                           
+            char[] pass = new char[username.Length];
+
+            for (int i = 0, j = username.Length - 1; i < username.Length; i++, j--)
+            {                
+                pass[i] = username[j]; 
+            }
+            
+            string reversed = string.Join("", pass); 
+
+            while (true)
+            {
+                string correctPassword = Console.ReadLine(); 
+                
+                if (correctPassword == reversed)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+
+                Console.WriteLine("Incorrect password. Try again.");               
+            }
+        }
+    }
+}
